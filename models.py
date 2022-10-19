4f26ac29074e25fca71f7ffc411cf3babbebbaa0
Dflint08 <100096400+Dflint08@users.noreply.github.com>
2022-10-19 16:05:09 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length= 50)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    title = models.CharField(max_length= 1000)
+    date_released = models.DateField(default= datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.CharField(max_length= 400)
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.CharField(max_length=1000)
+    song_id = models.CharField(max_length= 400)
--- views.py
+++ views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Yayyy")
+    return HttpResponse("Hello, this is my first django app")
